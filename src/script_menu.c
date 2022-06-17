b3583c5ef9cd5a52b26125986b4aac8552965177
Lt. Debaser <lieutenant_debaser@outlook.com>
2022-06-17 10:08:51 -0400

    Removed Mystery Event Flag Checks at SS Tidal
    Flag checks for Mystery Event items at the SS Tidal have been removed
    and now only check if the item is in the player's inventory.

--- src/script_menu.c
+++ src/script_menu.c
@@ -428,7 +428,7 @@ static void CreateLilycoveSSTidalMultichoice(void)
         }
     }
 
-    if (CheckBagHasItem(ITEM_EON_TICKET, 1) == TRUE && FlagGet(FLAG_ENABLE_SHIP_SOUTHERN_ISLAND) == TRUE)
+    if (CheckBagHasItem(ITEM_EON_TICKET, 1) == TRUE)
     {
         if (gSpecialVar_0x8004 == 0)
         {
@@ -444,7 +444,7 @@ static void CreateLilycoveSSTidalMultichoice(void)
         }
     }
 
-    if (CheckBagHasItem(ITEM_MYSTIC_TICKET, 1) == TRUE && FlagGet(FLAG_ENABLE_SHIP_NAVEL_ROCK) == TRUE)
+    if (CheckBagHasItem(ITEM_MYSTIC_TICKET, 1) == TRUE)
     {
         if (gSpecialVar_0x8004 == 0)
         {
@@ -460,7 +460,7 @@ static void CreateLilycoveSSTidalMultichoice(void)
         }
     }
 
-    if (CheckBagHasItem(ITEM_AURORA_TICKET, 1) == TRUE && FlagGet(FLAG_ENABLE_SHIP_BIRTH_ISLAND) == TRUE)
+    if (CheckBagHasItem(ITEM_AURORA_TICKET, 1) == TRUE)
     {
         if (gSpecialVar_0x8004 == 0)
         {
@@ -476,7 +476,7 @@ static void CreateLilycoveSSTidalMultichoice(void)
         }
     }
 
-    if (CheckBagHasItem(ITEM_OLD_SEA_MAP, 1) == TRUE && FlagGet(FLAG_ENABLE_SHIP_FARAWAY_ISLAND) == TRUE)
+    if (CheckBagHasItem(ITEM_OLD_SEA_MAP, 1) == TRUE)
     {
         if (gSpecialVar_0x8004 == 0)
         {
