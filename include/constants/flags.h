56aad1962d6186b60ffd14bf8576d6e2ce63e1f9
Lt. Debaser <lieutenant_debaser@outlook.com>
2022-07-24 07:46:46 -0400

    Scripting for Ever Grande City Guard
    Untested scripting for a new guard by the entrance to the ELITE FOUR.
    This area will be a new cave that houses a boss Pokemon.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -96,8 +96,8 @@
 #define FLAG_RESCUED_BIRCH                       0x52
 #define FLAG_LEGENDARIES_IN_SOOTOPOLIS           0x53
 
-#define FLAG_UNUSED_0x054                    0x54  // Unused Flag
-#define FLAG_UNUSED_0x055                    0x55  // Unused Flag
+#define FLAG_EVERGRANDE_GUARD_MOVED              0x54  // Should guard in Ever Grande city move
+#define FLAG_UNUSED_0x055                        0x55  // Unused Flag
 
 #define FLAG_HIDE_CONTEST_POKE_BALL          0x56  // Always set after new game, object it hides is added directly
 #define FLAG_MET_RIVAL_MOM                   0x57
